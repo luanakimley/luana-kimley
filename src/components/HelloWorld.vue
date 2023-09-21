24a3bce9c52ce68b8ad9c27d559a04d0b9b2393d
luanakimley <luanaa.kimley@gmail.com>
2023-09-21 16:51:18 +0100

    :lipstick: Add styling and animation to links

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,17 +18,62 @@ defineProps<{
       height="175"
     />
 
-    <h1 class="green">Hi There!</h1>
-    <h3 class="mb-4">I'm Luana</h3>
+    <h3 class="fw-bold outline yellow text-uppercase">Hi There!</h3>
+    <h1 class="mb-4">I'm <strong class="blue">Luana</strong></h1>
 
-    <RouterLink class="" to="/about">About Me</RouterLink>
-    <RouterLink to="/projects">My Projects</RouterLink>
+    <div class="d-flex flex-column text-center">
+      <RouterLink class="my-3 pink nav-link" to="/about">about me</RouterLink>
+      <RouterLink class="pink nav-link" to="/projects">my projects</RouterLink>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .vh-100 {
   height: 100vh;
-  overflow: hidden;
+}
+
+.nav-link {
+  cursor: pointer;
+  position: relative;
+  padding: 10px 20px;
+  background: white;
+  border-top-right-radius: 10px;
+  border-bottom-left-radius: 10px;
+  transition: all 1s;
+  &:after,
+  &:before {
+    content: ' ';
+    width: 10px;
+    height: 10px;
+    position: absolute;
+    border: 0px solid #fff;
+    transition: all 1s;
+  }
+  &:after {
+    top: -1px;
+    left: -1px;
+    border-top: 3px solid #ffc612;
+    border-left: 3px solid #2671bc;
+  }
+  &:before {
+    bottom: -1px;
+    right: -1px;
+    border-bottom: 3px solid #ffc612;
+    border-right: 3px solid #2671bc;
+  }
+  &:hover {
+    border-top-right-radius: 0px;
+    border-bottom-left-radius: 0px;
+    &:before,
+    &:after {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+
+.outline {
+  font-family: 'Montserrat Subrayada', sans-serif;
 }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,27 @@
 <template>
   <nav class="nav-bar">
     <div class="d-flex justify-content-center">
-      <a class="px-3">home</a>
-      <a class="px-3">about</a>
-      <a class="px-3">projects</a>
+      <RouterLink to="/" class="mx-2 pink nav-link">home</RouterLink>
+      <RouterLink to="/about" class="mx-2 pink nav-link">about</RouterLink>
+      <RouterLink to="/projects" class="mx-2 pink nav-link">projects</RouterLink>
     </div>
   </nav>
 </template>
 <style scoped>
 .nav-bar {
   margin: 10px;
+  margin-top: 20px;
+}
+
+.nav-link {
+  cursor: pointer;
+  position: relative;
+  transition: all 0.7s;
+  padding: 5px 10px;
+  border: 3px solid transparent;
+  &:hover {
+    border-bottom: 3px solid #2671bc;
+    border-top: 3px solid #ffc612;
+  }
 }
 </style>
