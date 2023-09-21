12b09676c42d3e1a686a6af1a7b504c6cad66de6
luanakimley <luanaa.kimley@gmail.com>
2023-09-21 13:29:40 +0100

    :tada: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld class="home-page" msg="Luana Kimley" />
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="d-flex justify-content-center align-items-center flex-column vh-100">
+    <img
+      class="rounded-circle mb-4"
+      alt="Luana picture"
+      src="@/assets/profile.jpg"
+      width="150"
+      height="150"
+    />
+
+    <h1 class="green">Hi There!</h1>
+    <h3 class="mb-4">I'm Luana</h3>
+
+    <RouterLink class="" to="/about">About Me</RouterLink>
+    <RouterLink to="/projects">My Projects</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+.vh-100 {
+  height: 100vh;
+}
+</style>
